dc6eabd3a3fcc1da8c5f1b3cfdc24593b30d1b58
AnneKsatn <AnneKsatn@gmail.com>
2023-05-05 13:30:05 +0300

    fixed cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,34 +10,27 @@ import 'react-before-after-slider-component/dist/build.css'
 
 export default function Home() {
 
-    const FIRST_IMAGE = {
-        imageUrl: 'https://sun9-69.userapi.com/impf/EpanNJQEv6I5kMNXI-67oJ4TXy4ACMO-8ttP_w/x1Cd-6iQ8k0.jpg?size=1920x768&quality=95&crop=0,164,2048,818&sign=7d79a14b234685badc2e399db5f9b69e&type=cover_group'
-    }
-    const SECOND_IMAGE = {
-        imageUrl: 'https://sun9-69.userapi.com/impf/EpanNJQEv6I5kMNXI-67oJ4TXy4ACMO-8ttP_w/x1Cd-6iQ8k0.jpg?size=1920x768&quality=95&crop=0,164,2048,818&sign=7d79a14b234685badc2e399db5f9b69e&type=cover_group'
-    }
-
     const [networks, setNetworks] = useState([
         {
-            title: "Controlnet Hough",
-            description: "Controlnet Hough генерирует дизайны интерьеров.",
+            title: "Neurite",
+            description: "Анализ медицинских изображений",
             status: true,
-            herf: "/service",
-            src: 'https://techbriefly.com/wp-content/uploads/2022/11/Interior-AI-5.jpg'
+            herf: "",
+            src: 'https://static.tildacdn.com/tild3139-3530-4336-a534-326461356336/chest-5.jpg'
         },
         {
-            title: "Image Restoration",
-            description: "Image Restoration отреставрирует старые фотографии.",
+            title: "Pneumonia-Detection",
+            description: "Распознавание пневмонии по рентгену",
             status: false,
             herf: "",
-            src: 'https://replicate.delivery/mgxm/a79d403f-ab71-4a23-a976-59a25341dcb3/out.png'
+            src: 'https://static.tildacdn.com/tild3139-3530-4336-a534-326461356336/chest-5.jpg'
         },
         {
-            title: "Super Resolution",
-            description: "Super Resolution восстановит качество размытой фотографии",
+            title: "Cancer-Detection",
+            description: "Распознавание рака по рентгену",
             status: false,
             herf: "",
-            src: "https://tjzk.replicate.delivery/models_models_cover_image/aabde67b-bf5c-4fc8-a4bd-8b2dcba60be6/swin2sr-cover3.png"
+            src: "https://static.tildacdn.com/tild3139-3530-4336-a534-326461356336/chest-5.jpg"
         },
     ])
 
@@ -51,10 +44,10 @@ export default function Home() {
 
                 <div className={styles.subheader}>
                     <p>
-                        Маркетплейс <br /> моделей искуственного интеллекта
+                        Диспансеризация <br /> с помощью искуственного интеллекта
                     </p>
                 </div>
-                <p className={styles.call_to_action}>Получите доступ ко всем возможностям ИИ одним нажатием кнопки <br />или разверните свою модель машинного обучения за считанные минуты.
+                <p className={styles.call_to_action}>Пройдите полное обследование с помощью ИИ за считанные минуты.
                 </p>
 
             </div>
